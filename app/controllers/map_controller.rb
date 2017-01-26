d5261c6f288e2e3facb475783b1836d02e8f66b4
MacTHEgenius <levesque.benoit@me.com>
2017-01-26 13:36:55 -0500

    Add gems and map controller

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,5 +1,11 @@
 class MapController < ApplicationController
 
+  respond_to :json
 
+  def index
+    file = File.read('map.json')
+    hash = JSON.parse file
+    render json: hash, status: 200
+  end
 
 end
