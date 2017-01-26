0deca966d87f0caedb5e31f02c336989a23d6a08
MacTHEgenius <levesque.benoit@me.com>
2017-01-26 14:48:36 -0500

    Delete api subdomain (Heroku errors)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
 
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json } do
 
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
 
