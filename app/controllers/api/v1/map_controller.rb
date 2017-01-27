0f01503574c0da31e675cfce35d2a0d3bafcbf39
MacTHEgenius <levesque.benoit@me.com>
2017-01-27 11:09:07 -0500

    Add Swagger for documentation

--- app/controllers/api/v1/map_controller.rb
+++ app/controllers/api/v1/map_controller.rb
@@ -2,6 +2,15 @@ class Api::V1::MapController < ApplicationController
 
   respond_to :json
 
+  swagger_controller :map, 'Map'
+
+  swagger_api :index do
+    summary "Fetch the map infos"
+    response :ok, "Success", :Map
+    response :unauthorized
+    response :not_acceptable
+  end
+
   def index
     file = File.read('map.json')
     hash = JSON.parse file
---
+++ config/initializers/swagger_docs.rb
@@ -0,0 +1,23 @@
+Swagger::Docs::Config.register_apis({
+    '1.0' =>  {
+        #:controller_base_path => "/app/controllers/api/v1",
+        # the extension used for the API
+        :api_extension_type => :json,
+        # the output location where your .json files are written to
+        :api_file_path => "public",
+        # the URL base path to your API
+        :base_path => "http://localhost:3000",
+        :host => "https://csf-geo-app.herokuapp.com/",
+        # if you want to delete all .json files at each generation
+        :clean_directory => true,
+        # add custom attributes to api-docs
+        :attributes => {
+            :info => {
+                "title" => "GeoAPI",
+                "description" => "Rails API documention with Swagger UI.",
+                "termsOfServiceUrl" => "http://someurl.com/",
+                "contact" => "an.email@example.com"
+            }
+        }
+    }
+})
