2c65d65eca2817f67ef89a2a84321ab86023f542
MacTHEgenius <levesque.benoit@me.com>
2017-01-30 10:58:42 -0500

    No master admin can't delete or edit other admins

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -25,7 +25,7 @@ class LoginsController < ApplicationController
   def destroy
     session[:admin_id] = nil
     flash[:success] = "You have logged out"
-    redirect_to root_path
+    redirect_to login_path
   end
 
 end
