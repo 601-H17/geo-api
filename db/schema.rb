84a20faf72d75763db1e8977dea217e84d20b992
Fred Laberge <fred.laberge7@gmail.com>
2017-01-31 09:54:09 -0500

    Update map.json

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130142925) do
+ActiveRecord::Schema.define(version: 20170130190918) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "username"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170130142925) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+  end
+
+  create_table "classrooms", force: :cascade do |t|
+    t.string   "name",        default: ""
+    t.string   "description", default: ""
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
