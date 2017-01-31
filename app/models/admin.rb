284796c769ef0adaa1fa711a99326e1d09462712
MacTHEgenius <levesque.benoit@me.com>
2017-01-31 10:34:01 -0500

    Add admin model tests

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,6 +2,8 @@ class Admin < ActiveRecord::Base
 
   REGEX_EMAIL = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
+  before_save { self.email = email.downcase }
+
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 30}
   validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 200}, format: {with: REGEX_EMAIL}
 
