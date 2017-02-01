2abe41e7b25ce630efd8f6c8d94224070bdef089
MacTHEgenius <levesque.benoit@me.com>
2017-02-01 11:32:34 -0500

    Add validations to classroom model

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,2 +1,8 @@
 class Classroom < ActiveRecord::Base
+
+  REGEX_NAME = /[A-Z]-[0-9]{3}/
+
+  validates :name, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 5, maximum: 5}, format: {with: REGEX_NAME}
+  validates :description, presence: true, length: {maximum: 300}
+
 end
