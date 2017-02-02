880dbbede400b806505e9479f78b4a5762b7e4c9
MacTHEgenius <levesque.benoit@me.com>
2017-02-02 13:19:19 -0500

    Pathfinder url in env variable

--- app/controllers/api/v1/path_controller.rb
+++ app/controllers/api/v1/path_controller.rb
@@ -3,7 +3,9 @@ require 'httparty'
 class Api::V1::PathController < ApplicationController
   include ClassroomsHelper
 
-  PATHFINDER_API_URL = 'http://localhost:8080/api/pathfinder?'
+  rescue_from Errno::ECONNREFUSED, :with => :refused
+
+  PATHFINDER_API_URL = ENV['PATHFINDER_URL']
 
   respond_to :json
 
@@ -11,8 +13,15 @@ class Api::V1::PathController < ApplicationController
     local_a = params[:localA]
     local_b = params[:localB]
     puts validate(local_a) && validate(local_b)
-    response = HTTParty.get("#{PATHFINDER_API_URL}localA=#{local_a}&localB=#{local_b}")
+
+    puts
+    response = HTTParty.get("#{PATHFINDER_API_URL}?localA=#{local_a}&localB=#{local_b}")
     render json: response, status: 200
   end
+
+  private
+    def refused
+      render json: { error_message: "Server could not fulfilled the request, path finding is unavailable. Try it later." }, status: 503
+    end
 
 end
