aa354e53fa64c2fb156e56a77094cd2eea384142
MacTHEgenius <levesque.benoit@me.com>
2017-02-10 14:33:50 -0500

    Fix issues

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -7,10 +7,29 @@ class TokensController < ApplicationController
     @tokens = ApiKey.all
   end
 
+  # POST /tokens
+
+  def create
+    @token = ApiKey.new(token_params)
+    if @token.save
+      flash[:success] = "Token was successfully generated: <b>#{@token.access_token}</b>"
+      redirect_to token_path(@token)
+    else
+      render 'new'
+    end
+  end
+
   # GET /tokens/new
 
   def new
     @token = ApiKey.new
   end
+
+  # /tokens/:id
+
+  private
+    def token_params
+      params.require(:api_key).permit(:name)
+    end
 
 end
