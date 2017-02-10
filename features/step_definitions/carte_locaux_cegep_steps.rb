298514ec9ae568940d040d4b76c8af3793e12d6e
Fred Laberge <fred.laberge7@gmail.com>
2017-02-10 09:58:07 -0500

    Add test integration

--- features/step_definitions/carte_locaux_cegep_steps.rb
+++ features/step_definitions/carte_locaux_cegep_steps.rb
@@ -1,5 +1,8 @@
 When(/^Les locaux sur affichés$/) do
+  visit map_path
 end
 
 Then(/^Valide la carte$/) do
+  # Le bouton Validate sera fait dans la user story de téléversement de la carte
+  # should have_content("Link Clicked")
 end
