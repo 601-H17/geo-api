f8fe7e0ed8f5650e651a0ee902c95df40c738519
Fred Laberge <fred.laberge7@gmail.com>
2017-02-15 09:09:33 -0500

    Add uploader

---
+++ app/controllers/upload_controller.rb
@@ -0,0 +1,14 @@
+class UploadController < ApplicationController
+
+  def show
+    # mapUploader.retrieve_from_store!('test.json')
+  end
+
+  def upload
+    mapUploader = MapUploader.new
+    map_file = params[:map]
+    mapUploader.store!(map_file)
+    flash[:success] = "Successfully upload the file"
+  end
+
+end
