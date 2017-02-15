553aa5ffa0d13ad0c623363d47e211acf778f568
MacTHEgenius <levesque.benoit@me.com>
2017-02-15 10:45:22 -0500

    Begin tokens controller tests

--- test/controllers/tokens_controller_test.rb
+++ test/controllers/tokens_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class TokensControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @admin = admins(:one)
+    @api_key1 = api_keys(:one)
+    @api_key2 = api_keys(:two)
+  end
+
+  test "get tokens index with admin already sign in" do
+    session[:admin_id] = @admin.id
+
+    get :index
+    assert_response :success
+  end
+
 end
