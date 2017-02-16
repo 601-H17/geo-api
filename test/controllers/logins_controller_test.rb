0041a317c9142ecbffe547ea1a56438ccd8536dd
Fred Laberge <fred.laberge7@gmail.com>
2017-02-16 13:15:26 -0500

    Add login controller unit tests

--- test/controllers/logins_controller_test.rb
+++ test/controllers/logins_controller_test.rb
@@ -1,5 +1,24 @@
 class LoginsControllerTest < ActionController::TestCase
 
+  setup do
+    @master_admin = admins(:one)
+    @no_master_admin = admins(:two)
+  end
 
+  test "should sucess if connect" do
+    session[:admin_id] = @master_admin.id
+    get :new
+    assert_response :redirect
+  end
+
+  test "should stay on page if error happens" do
+    get :new
+    assert_response :success
+  end
+
+  test "should destroy admin" do
+    delete :destroy, id: @master_admin
+    assert_response :redirect
+  end
 
 end
