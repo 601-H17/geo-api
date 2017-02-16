68732727f3882459b809e33a2fab490e1ca1de5a
Benoit Levesque <levesque.benoit@me.com>
2017-02-16 08:26:37 -0500

    Add index test for tokens controller

--- test/controllers/tokens_controller_test.rb
+++ test/controllers/tokens_controller_test.rb
@@ -6,13 +6,23 @@ class TokensControllerTest < ActionController::TestCase
     @admin = admins(:one)
     @api_key1 = api_keys(:one)
     @api_key2 = api_keys(:two)
+
+    session[:admin_id] = @admin.id
+  end
+
+  test "admin not sign in access" do
+    session[:admin_id] = nil
+
+    get :index
+    assert_response :redirect
+    assert_redirected_to root_path
   end
 
   test "get tokens index with admin already sign in" do
-    session[:admin_id] = @admin.id
-
     get :index
     assert_response :success
+    assert_match @api_key1.name, @response.body
+    assert_match @api_key2.name, @response.body
   end
 
 end
