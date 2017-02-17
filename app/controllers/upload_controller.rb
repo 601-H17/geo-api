fd3ee9c0a2a3a0117498c98813daf6cdfd1d035c
Fred Laberge <fred.laberge7@gmail.com>
2017-02-17 12:34:15 -0500

    Resolve conflict

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -8,10 +8,7 @@ class UploadController < ApplicationController
     mapUploader = MapUploader.new
     map_file = params[:map]
     mapUploader.store!(map_file)
-<<<<<<< HEAD
-=======
     flash[:success] = "Successfully upload the file"
->>>>>>> master
   end
 
 end
