e42a86ef1e39eafb503af9bc59b7c89e52fe30b4
MacTHEgenius <levesque.benoit@me.com>
2017-02-22 16:52:56 -0500

    Complete tests for corridors (api)

--- test/controllers/api/v1/corridors_controller_test.rb
+++ test/controllers/api/v1/corridors_controller_test.rb
@@ -2,6 +2,22 @@ require 'test_helper'
 
 class Api::V1::CorridorsControllerTest < ActionController::TestCase
 
+  setup do
+    @token = api_keys(:one)
+    @request.headers["Authorization"] = "Token token=#{@token.access_token}"
+  end
+
+  test "should not get without token" do
+    @request.headers["Authorization"] = nil
+
+    get :index
+
+    puts "Response: #{@response.body}"
+    assert_response :unauthorized
+    assert_equal "HTTP Token: Access denied.\n", @response.body
+
+  end
+
   test "should get corridors json" do
     get :index
 
