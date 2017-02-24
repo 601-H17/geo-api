a6b5bdf275a35c52a7cbcbd025aeb4e15cff5fb6
MacTHEgenius <levesque.benoit@me.com>
2017-02-24 09:09:59 -0500

    Add api-docs.json file reading and swagger-ui to get it

--- app/controllers/api/v1/docs_controller.rb
+++ app/controllers/api/v1/docs_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::DocsController < ApplicationController
 
   respond_to :json
 
+  def index
+    path = Rails.public_path + 'api-docs.json'
+    hash = parse(path)
+    render json: hash, status: 200
+  end
+
 end
