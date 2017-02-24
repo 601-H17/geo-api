22a7680d8de8c79a21fdcfb3cd87c93a0983898f
MacTHEgenius <levesque.benoit@me.com>
2017-02-24 12:17:22 -0500

    Trying to fix swagger issue

--- app/controllers/api/v1/docs_controller.rb
+++ app/controllers/api/v1/docs_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::DocsController < ApplicationController
   respond_to :json
 
   def index
-    path = Rails.public_path + 'api-docs.json'
+    path = Rails.public_path + "api-docs.json"
+    puts path
     hash = parse(path)
     render json: hash, status: 200
   end
