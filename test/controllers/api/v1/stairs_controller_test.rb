ee195d2f9c93def9daee1f5805c669649711927e
MacTHEgenius <levesque.benoit@me.com>
2017-02-27 13:59:16 -0500

    Fix test issue

--- test/controllers/api/v1/stairs_controller_test.rb
+++ test/controllers/api/v1/stairs_controller_test.rb
@@ -1,12 +1,32 @@
-require 'rails_helper'
+require 'test_helper'
 
-RSpec.describe StairsController, type: :controller do
+class Api::V1::StairsControllerTest < ActionController::TestCase
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
+  setup do
+    @stair = stairs(:one)
+    @point = points(:one)
+    @stair.point = @point
+    @token = api_keys(:one)
+    @request.headers["Authorization"] = "Token token=#{@token.access_token}"
+  end
+
+  test "should not get stairs without token" do
+    @request.headers["Authorization"] = nil
+
+    get :index
+
+    assert_response :unauthorized
+    assert_nil request.headers["Authorization"]
+  end
+
+  # Index (GET)
+
+  test "should get stairs with token" do
+    get :index
+
+    assert_response :success
+    assert_includes request.headers["Authorization"], @token.access_token
+    #assert_match @stair.to_json(include: { point: {only: [:lat, :lng]} }, except: :point_id), @response.body
   end
 
 end
