819ffd12362bbf01290783e7a42df43f877e2cb9
Fred Laberge <fred.laberge7@gmail.com>
2017-02-27 09:48:34 -0500

    Add validation in the model

--- app/models/map.rb
+++ app/models/map.rb
@@ -3,4 +3,11 @@ class Map < ActiveRecord::Base
   mount_uploader :map, MapUploader
 
   validates :name, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 15}
+
+  validates :map, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 150}
+
+  validates :currentMap, presence: true
+
+  validates :floor, presence: true, numericality: {only_integer: true}, greater_than: 0, less_than: 4
+
 end
