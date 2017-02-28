59aea24274f37e8ecac3aecb74ff0ff2b2a7b11d
MacTHEgenius <levesque.benoit@me.com>
2017-02-28 09:56:03 -0500

    Add test and wing for classroom

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,14 +1,13 @@
 class Classroom < ActiveRecord::Base
 
   REGEX_NAME = /[A-Z]-[0-9]{3}/
+  REGEX_WING = /[A-Z]/
 
   validates :name, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 5, maximum: 5}, format: {with: REGEX_NAME}
   validates :description, length: {maximum: 300}
-
-  # TODO: ! To test ! #
-  # validates :floor, presence: true
-  # validates :point, presence: true
-  # validates :wing, presence: true, length: {minimum: 1, maximum: 1}
+  validates :wing, presence: true, length: {minimum: 1, maximum: 1}, format: {with: REGEX_WING}
+  validates :floor, presence: true, numericality: {only_integer: true}
+  validates :point, presence: true
 
   belongs_to :point
 
