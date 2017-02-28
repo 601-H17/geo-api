3fae660654bc2c2754f5086c61827ad54c0284c7
MacTHEgenius <levesque.benoit@me.com>
2017-02-28 10:25:45 -0500

    Add test for new show_by_name stair controller

--- app/controllers/api/v1/stairs_controller.rb
+++ app/controllers/api/v1/stairs_controller.rb
@@ -11,4 +11,6 @@ class Api::V1::StairsController < ApplicationController
     render json: stairs.to_json(include: { point: {only: [:lat, :lng]} }, except: :point_id), status: 200
   end
 
+
+
 end
--- test/controllers/api/v1/stairs_controller_test.rb
+++ test/controllers/api/v1/stairs_controller_test.rb
@@ -29,4 +29,24 @@ class Api::V1::StairsControllerTest < ActionController::TestCase
     #assert_match @stair.to_json(include: { point: {only: [:lat, :lng]} }, except: :point_id), @response.body
   end
 
+  # Show by name (GET by name)
+
+  test "should get stair by name with token" do
+    get :show_by_name, name: "GE-1"
+
+    assert_response :success
+    assert_includes request.headers["Authorization"], @token.access_token
+    assert_match @stair.to_json(include: { point: {only: [:lat, :lng]} }, except: :point_id).to_s, @response.body.to_s
+  end
+
+  test "should not get stair, get error 404 with token and unknown name" do
+    name = "GE-10"
+
+    get :show_by_name, name: name
+
+    assert_response :not_found
+    assert_includes request.headers["Authorization"], @token.access_token
+    assert_match "Stair  #{name} not found", @response.body.to_s
+  end
+
 end
