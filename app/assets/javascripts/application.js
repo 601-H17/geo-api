c020fdefe479a7752a9a8dda7655336dc4dae2c0
Fred Laberge <fred.laberge7@gmail.com>
2017-02-28 08:56:47 -0500

    Install materialize and made login page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require bootstrap-sprockets
-//= require turbolinks
+//= require materialize-sprockets
 //= require_tree .
+//= require turbolinks
