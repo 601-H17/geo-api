705d40621756fe4c10e35057f25ae281baa37a1a
MacTHEgenius <levesque.benoit@me.com>
2017-03-07 10:11:57 -0500

    Add validation and test for tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,6 @@ class Tag < ActiveRecord::Base
 
   has_and_belongs_to_many :classrooms
 
+  validates :name, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 20}
+
 end
